39c4e3112bbdc84f0020211da7144dd42852a556
Sebastian Karlsen <sebastian@karlsen.fr>
2022-06-17 01:21:14 +0200

    Make tabs consistent

--- index.php
+++ index.php
@@ -1,34 +1,34 @@
 <!doctype html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8">
+    <head>
+        <meta charset="utf-8">
 
-		<title>karlsen.fr</title>
-		<meta name="description" content="Sebastian Karlsen's portfolio">
-		<meta name="keywords" content="karlsen, sebastian, portfolio">
-		<meta name="author" content="Sebastian Karlsen">
+        <title>karlsen.fr</title>
+        <meta name="description" content="Sebastian Karlsen's portfolio">
+        <meta name="keywords" content="karlsen, sebastian, portfolio">
+        <meta name="author" content="Sebastian Karlsen">
 
         <link rel="stylesheet" href="style.css">
         <link rel="icon" type="image/x-icon" href="icons/favicon.png">
-	</head>
+    </head>
 
-	<body>
-		<table>
-			<tr class="prompt">
-				<td class="username"><p>sebastian</p></td>
-				<td><p>@</p></td>
-				<td class="hostname"><pre><?php require_once('karlsen.fr.ascii'); ?></pre></td>
-				<td><p>:</p></td>
-				<td class="dir"><p>~</p></td>
-			</tr>
+    <body>
+        <table>
+            <tr class="prompt">
+                <td class="username"><p>sebastian</p></td>
+                <td><p>@</p></td>
+                <td class="hostname"><pre><?php require_once('karlsen.fr.ascii'); ?></pre></td>
+                <td><p>:</p></td>
+                <td class="dir"><p>~</p></td>
+            </tr>
 
-			<tr class="links">
-				<td colspan="5">
-					<a href="https://github.com/sebastka" title="Github" target="_blank"><img src="icons/github.invert.svg" alt="Github"></a>
-					<a href="https://www.linkedin.com/in/sebastka/" title="LinkedIn" target="_blank"><img src="icons/linkedin.invert.svg" alt="LinkedIn"></a>
-					<a href="public.asc" title="PGP Public key"><img src="icons/key.invert.svg" alt="PGP"></a>
-				</td>
-			</tr>
-		</table>
-	</body>
+            <tr class="links">
+                <td colspan="5">
+                    <a href="https://github.com/sebastka" title="Github" target="_blank"><img src="icons/github.invert.svg" alt="Github"></a>
+                    <a href="https://www.linkedin.com/in/sebastka/" title="LinkedIn" target="_blank"><img src="icons/linkedin.invert.svg" alt="LinkedIn"></a>
+                    <a href="public.asc" title="PGP Public key"><img src="icons/key.invert.svg" alt="PGP"></a>
+                </td>
+            </tr>
+        </table>
+    </body>
 </html>
